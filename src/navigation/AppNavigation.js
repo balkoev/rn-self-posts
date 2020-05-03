134afbcd9f0974e6da921049eb39ab82ce415c56
Adam <adam22185515@gmail.com>
2020-05-03 16:30:37 +0300

    added first header icons

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { Platform } from 'react-native';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
 import { THEME } from '../theme';
+import { AppHeaderIcon } from '../components/AppHeaderIcon';
 
 const Stack = createStackNavigator();
 
@@ -25,6 +27,20 @@ export const AppNavigation = () => (
         component={MainScreen}
         options={{
           headerShown: true,
+          headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+              <Item
+                title="Take photo"
+                iconName="ios-camera"
+                onPress={() => console.log('press')}
+              />
+              <Item
+                title="Take photo 2"
+                iconName="ios-reverse-camera"
+                onPress={() => console.log('press')}
+              />
+            </HeaderButtons>
+          ),
         }}
       />
       <Stack.Screen name="Post" component={PostScreen} />
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import { THEME } from '../theme';
+import { DATA } from '../data';
 
 export const PostScreen = ({ navigation, route }) => {
   const postId = route.params.postId;
@@ -9,17 +19,51 @@ export const PostScreen = ({ navigation, route }) => {
     title: 'Пост от ' + new Date(date).toLocaleDateString(),
   });
 
+  const post = DATA.find((p) => p.id === postId);
+
+  const removeHandler = () => {
+    Alert.alert(
+      'Удаление поста',
+      'Вы точно хотите удалить пост?',
+      [
+        {
+          text: 'Отменить',
+          style: 'cancel',
+        },
+        {
+          text: 'Удалить',
+          style: 'destuctive',
+          onPress: () => console.log('OK Pressed'),
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
-    <View style={styles.center}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: post.img }} style={styles.image}></Image>
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button
+        title="удалить"
+        color={THEME.DANGER_COLOR}
+        onPress={removeHandler}
+      />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  textWrap: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: 'open-regular',
   },
 });
